65661e522d4b3f87427c42f19c93d2d74abdf79f
宁子希 <1589326497@qq.com>
2024-02-18 10:56:25 +0800

    1

--- lx.cpp
+++ lx.cpp
@@ -1,6 +1,23 @@
+/**
+ * @file lx.cpp
+ * @author 宁子希 (1589326497@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include <iostream>
 using namespace std;
 //函数模板
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ * @param a 
+ * @param b 
+ */
 template<typename T>
 void mySwap(T &a, T &b) {
     T temp = a;
